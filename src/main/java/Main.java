aafee977c14f401289c97e4eb29d9ea32c3c51cf
Ashoka Shringla <ashokashringla@gmail.com>
2022-02-03 14:43:41 -0500

    add files

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import io.javalin.Javalin;
+import java.util.*;
+import io.javalin.http.Handler;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Javalin app = Javalin.create().start(4100);
+
+        app.get("/", ctx -> ctx.render("index.jte"));
+
+        app.get("/getEmail", getFormHandler);
+
+        app.get("/postEmail", postFormHandler);
+
+    }
+
+    public static Handler getFormHandler = ctx -> {
+
+        ctx.render("form.jte");
+
+    };
+
+    public static Handler postFormHandler = ctx -> {
+
+        String sender = ctx.formParam("sender");
+        String receiver = ctx.formParam("receiver");
+        String subject = ctx.formParam("subject");
+        String contents = ctx.formParam("contents");
+
+        ctx.result("You are sending " + receiver + " an email from " + sender + " \nwith the subject: " 
+                    + subject + " \nand the contents: " + contents);
+
+    };
+
+}
